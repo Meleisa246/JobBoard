2fc27d399fc1bafe86dcb17182f320a53ddbe6a3
Meleisa246 <meleisa246@yahoo.com>
2023-12-16 19:33:24 -0500

    Update job-single.php
    update

--- jobs/job-single.php
+++ jobs/job-single.php
@@ -101,7 +101,7 @@
 
 
 ?>
-    <!-- HOME -->
+    
     <section class="section-hero overlay inner-page bg-image" style="background-image: url('../images/hero_1.jpg');" id="home-section">
       <div class="container">
         <div class="row">
